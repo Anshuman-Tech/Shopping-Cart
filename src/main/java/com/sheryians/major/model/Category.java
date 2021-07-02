a23367f8ea9155cfa053b543a3109ca9bc643951
Anshuman <anshuman20november@gmail.com>
2021-07-02 18:44:30 +0530

    Categories

--- src/main/java/com/sheryians/major/model/Category.java
+++ src/main/java/com/sheryians/major/model/Category.java
@@ -1,4 +1,15 @@
 package com.sheryians.major.model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "category_id")
+    private int id;
+    private String name;
+
 }
